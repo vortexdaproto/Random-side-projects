6df491163887ea54a74023157dc2dadaf9e9ffb4
vortexdaproto <jkblacknz@gmail.com>
2026-09-17 15:55:35 +1200

    Small syntax change

--- Code used for 247CTF/Cryptography/theimpossibleuser.js
+++ Code used for 247CTF/Cryptography/theimpossibleuser.js
@@ -1,7 +1,8 @@
 function toHex(str) {
 	let hex = "";
-	for(i=0;i<str.length;i++)
-			hex = hex + str.charCodeAt(i).toString(16);
+	for(i=0;i<str.length;i++) {
+		hex = hex + str.charCodeAt(i).toString(16);
+    }
 	console.debug(str.length + ": " + hex)
 	return hex
 }
