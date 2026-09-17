75ddbea12a460192aad91144c80619ac9b00c184
vortexdaproto <jkblacknz@gmail.com>
2026-09-17 15:52:13 +1200

    Solved The Impossible User on 247CTF

---
+++ Code used for 247CTF/Cryptography/theimpossibleuser.js
@@ -0,0 +1,16 @@
+function toHex(str) {
+	let hex = "";
+	for(i=0;i<str.length;i++)
+			hex = hex + str.charCodeAt(i).toString(16);
+	console.debug(str.length + ": " + hex)
+	return hex
+}
+console.debug("impossible_flag".length)
+console.debug("_user".length)
+toHex("impossible_flag")
+toHex("_user")
+/* Take both strings, encrypt them and take the first 32 chars of this.*/
+toHex("impossible_flagXuser") // Get the start of the encrypted string
+/* And the last 32 characters of this string when encrypted*/
+toHex("impossible_flaX_user") // Get the end of the encrypted string
+/* And you will get the flag.*/
